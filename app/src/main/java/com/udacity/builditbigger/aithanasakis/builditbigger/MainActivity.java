5f511b944dcbe046afb1d11acdf3fbd72547b79f
Thanos Ts <thanasakis@gmail.com>
2018-06-05 16:38:33 +0300

    working gce connected with java library

--- app/src/main/java/com/udacity/builditbigger/aithanasakis/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/builditbigger/aithanasakis/builditbigger/MainActivity.java
@@ -1,20 +1,71 @@
 package com.udacity.builditbigger.aithanasakis.builditbigger;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.builditbigger.aithanasakis.builditbigger.backend.myApi.MyApi;
 import com.udacity.builditbigger.aithanasakis.jokesmith.JokeSmith;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     JokeSmith jokeSmith;
+    TextView testing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView testing = (TextView) findViewById(R.id.testing);
+        testing = (TextView) findViewById(R.id.testing);
         jokeSmith = new JokeSmith();
-        testing.setText(jokeSmith.getJoke());
+        JokeTask jokeTask = new JokeTask();
+        jokeTask.execute();
+    }
+
+    public void showJoke(String joke){
+        testing.setText(joke);
+    }
+
+    public class JokeTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            showJoke(result);
+        }
+
     }
 }
--- backend/src/main/java/com/udacity/builditbigger/aithanasakis/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/builditbigger/aithanasakis/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.builditbigger.aithanasakis.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.udacity.builditbigger.aithanasakis.jokesmith.JokeSmith;
 
 import javax.inject.Named;
 
@@ -23,6 +24,15 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
+        MyBean response = new MyBean();
+        JokeSmith jokeSmith = new JokeSmith();
+        response.setData(jokeSmith.getJoke());
 
         return response;
     }
